5129d24367c2d2b97770749ed4fb57afb800446e
Angie Spears <angie.spears809@gmail.com>
2020-06-04 15:22:16 -0400

    add initial about me section

---
+++ gatsby-site/src/components/about/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './style/about.scss'
+
+const About = () => {
+  return (
+    <div className = 'about-container'>
+      <h1>About Me</h1>
+      <p> I 've worked with R/GA on projects for <b>Verizon</b> and <b>Samsung</b>.<br/> My Frontend experience include <b>React</b>, <b>Preact</b> and <b>Gatsby</b>. My backend experience includes <b>Node</b>, <b>Express</b>, <b>Postgress</b> and more. <span>I'm currently looking for an amazing position with a great company in NYC.</span> </p>
+    </div>
+  )
+}
+
+
+export default About
--- gatsby-site/src/components/about/intro.js
+++ gatsby-site/src/components/about/intro.js
@@ -1,5 +1,6 @@
 import React from "react"
 import './style/intro.scss'
+import AboutMe from './about.js'
 
 const Intro = () => {
   return (
@@ -8,6 +9,7 @@ const Intro = () => {
         <h1>Hi there!<br/>I'm Angie Spears</h1>
         <p>Full-stack Software Engineer proudly coding in New York City. I'm a Grace Hopper and General Assembly Alum.</p>
       </div>
+      <AboutMe/>
     </a>
   )
 }

e757694043c8b022bc848222be9866c66f3b72d8
Angie Spears <angie.spears809@gmail.com>
2020-06-04 11:16:02 -0400

    Add randomize menu bar background colors

--- gatsby-site/src/components/header/header.js
+++ gatsby-site/src/components/header/header.js
@@ -14,8 +14,10 @@ class Header extends Component {
 
   openNav() {
     const innerWidth = window.innerWidth
+    const colors = ['#F76259', '#92F759', '#59F7E5', '#5985F7', '#DF5FFA']
     if (innerWidth < 768) {
       this.menuItems.current.style.width = '100%'
+      this.menuItems.current.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
     }
   }
 

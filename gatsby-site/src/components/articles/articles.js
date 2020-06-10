2d05b88415b0ee75649540c3aff4ad064eaf9784
Angie Spears <angie.spears809@gmail.com>
2020-06-09 22:19:00 -0400

    add articles section and update style

---
+++ gatsby-site/src/components/articles/articles.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './articles.scss'
+
+
+const Articles = () => {
+  return (
+    <div className = 'articles-container'>
+      <h1>Articles</h1>
+      <a href= 'https://medium.com/@Angie.spears809/why-big-o-notation-a800320ea53c' target = '_blank'>Big O Notation</a>
+    </div>
+
+  )
+}
+
+export default Articles
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import About from '../components/about/about'
 import Projects from '../components/projects/projects.js'
+import Articles from '../components/articles/articles'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <About/>
     <Projects/>
+    <Articles/>
   </Layout>
 )
 

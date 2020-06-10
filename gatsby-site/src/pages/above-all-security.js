9a9addd3ba08298a0aaa4158bb136bb6fb6cb367
Angie Spears <angie.spears809@gmail.com>
2020-06-10 09:43:19 -0400

    create pages for all projects

--- gatsby-site/src/pages/above-all-security.js
+++ gatsby-site/src/pages/above-all-security.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout/layout.js"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const AboveAllSecurity = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Above All Security" />
+    <h1>Above All Security</h1>
+    <p>Content under construction</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default AboveAllSecurity

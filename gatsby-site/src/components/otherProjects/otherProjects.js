803af351a4744f090bddc79883f226d976ab3ed7
Angie Spears <angie.spears809@gmail.com>
2020-06-25 13:03:57 -0400

    update other projects mobile version

--- gatsby-site/src/components/otherProjects/otherProjects.js
+++ gatsby-site/src/components/otherProjects/otherProjects.js
@@ -5,28 +5,29 @@ import AboveAllSecurity  from '../projects/images/projects/above-all-tb.jpg'
 const OtherProjects = () => {
   return (
     <div className = 'other-projects-section-container'>
-      <h1>Other Projects</h1>
-      <div className = 'other-projects-container'>
-        <div className = 'other-projects'>
-          <img  className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
-        </div>
-        <div className = 'other-projects'>
-          <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'  />
-        </div>
-        <div className = 'other-projects'>
-          <img className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
-        </div>
-        <div className = 'other-projects'>
-          <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'/>
-        </div>
-        <div className = 'other-projects'>
-          <img className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
-        </div>
-        <div className = 'other-projects'>
-          <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'/>
+      <div>
+        <h1>Other Projects</h1>
+        <div className = 'other-projects-container'>
+          <div className = 'other-projects'>
+            <img  className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
+          </div>
+          <div className = 'other-projects'>
+            <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'  />
+          </div>
+          <div className = 'other-projects'>
+            <img className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
+          </div>
+          <div className = 'other-projects'>
+            <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'/>
+          </div>
+          <div className = 'other-projects'>
+            <img className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
+          </div>
+          <div className = 'other-projects'>
+            <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'/>
+          </div>
         </div>
       </div>
-
     </div>
   )
 }

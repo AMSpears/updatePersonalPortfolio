d92a24c6cf71b69bd8faa2b71cb4d5438127f8e0
Angie Spears <angie.spears809@gmail.com>
2020-06-24 21:47:48 -0400

    update about me style

--- gatsby-site/src/components/about/about.js
+++ gatsby-site/src/components/about/about.js
@@ -4,8 +4,10 @@ import './style/about.scss'
 const About = () => {
   return (
     <div className = 'about-container'>
-      <h1>About Me</h1>
-      <p> I&apos;ve worked with R/GA on projects for Verizon and Samsung. My Frontend experience include    React, Preact and Gatsby. My backend experience includes Node, Express, Postgress and more. I&apos;m currently looking for an amazing position with a great company in NYC. </p>
+      <div>
+        <h1>About Me</h1>
+        <p> I&apos;ve worked with R/GA on projects for Verizon and Samsung. My Frontend experience include    React, Preact and Gatsby. My backend experience includes Node, Express, Postgress and more. I&apos;m currently looking for an amazing position with a great company in NYC. </p>
+      </div>
     </div>
   )
 }

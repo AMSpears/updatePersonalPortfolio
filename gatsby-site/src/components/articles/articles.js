73d1c5d94338985751bb583e9a2fc617a827f1da
Angie Spears <angie.spears809@gmail.com>
2020-06-25 12:02:35 -0400

    update articles seccion

--- gatsby-site/src/components/articles/articles.js
+++ gatsby-site/src/components/articles/articles.js
@@ -5,10 +5,11 @@ import './articles.scss'
 const Articles = () => {
   return (
     <div id = 'medium' className = 'articles-container'>
-      <h1>Articles</h1>
-      <a href= 'https://medium.com/@Angie.spears809/why-big-o-notation-a800320ea53c' target = '_blank' rel='noreferrer'>Big O Notation</a>
+      <div>
+        <h1>Articles</h1>
+        <a href= 'https://medium.com/@Angie.spears809/why-big-o-notation-a800320ea53c' target = '_blank' rel='noreferrer'>Big O Notation</a>
+      </div>
     </div>
-
   )
 }
 

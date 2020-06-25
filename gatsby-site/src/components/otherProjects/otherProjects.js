a181d66ed7d2ee7c7b2659ad956bdae610bc91ce
Angie Spears <angie.spears809@gmail.com>
2020-06-25 11:58:01 -0400

    update img size

--- gatsby-site/src/components/otherProjects/otherProjects.js
+++ gatsby-site/src/components/otherProjects/otherProjects.js
@@ -4,26 +4,26 @@ import AboveAllSecurity  from '../projects/images/projects/above-all-tb.jpg'
 
 const OtherProjects = () => {
   return (
-    <div id = 'other-projects' className = 'other-projects-container'>
+    <div className = 'other-projects-section-container'>
       <h1>Other Projects</h1>
-      <div className = 'projects-img-container'>
+      <div className = 'other-projects-container'>
         <div className = 'other-projects'>
-          <img src = {AboveAllSecurity} alt = 'project-name'/>
+          <img  className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
         </div>
         <div className = 'other-projects'>
-          <img src = {AboveAllSecurity} alt = 'project-name'/>
+          <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'  />
         </div>
         <div className = 'other-projects'>
-          <img src = {AboveAllSecurity} alt = 'project-name'/>
+          <img className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
         </div>
         <div className = 'other-projects'>
-          <img src = {AboveAllSecurity} alt = 'project-name'/>
+          <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'/>
         </div>
         <div className = 'other-projects'>
-          <img src = {AboveAllSecurity} alt = 'project-name'/>
+          <img className = 'project-img even' src = {AboveAllSecurity} alt = 'project-name'/>
         </div>
         <div className = 'other-projects'>
-          <img src = {AboveAllSecurity} alt = 'project-name'/>
+          <img className = 'project-img odd' src = {AboveAllSecurity} alt = 'project-name'/>
         </div>
       </div>
 

ee0ef6029ae42104f2697758055a053a74942f98
Angie Spears <angie.spears809@gmail.com>
2020-06-25 22:58:22 -0400

    update main projects style

--- gatsby-site/src/components/projects/projects.js
+++ gatsby-site/src/components/projects/projects.js
@@ -11,43 +11,42 @@ import Provide_Services from '../../images/projects/provide-services.jpg'
 
 const Projects = () => {
   return (
-    <div id = 'projects' className = 'projects-container'>
-    <div>
-      <div><h1>Select Works</h1></div>
-
-     <div className = 'projects-imgs-container'>
-       <div className = 'project'>
-        <Link to = '/frs-verizon'>
-          <img src = {FRS_Project} alt = 'First Responders - Verizon'/>
-        </Link>
+    <div className = 'projects-container'>
+      <div>
+        <div><h1>Select Works</h1></div>
+        <div className = 'projects-inner-container'>
+          <div className = 'project'>
+            <Link to = '/frs-verizon'>
+              <img src = {FRS_Project} alt = 'First Responders - Verizon'/>
+            </Link>
+          </div>
+          <div className = 'project'>
+            <Link to = '/pentimento'>
+              <img src = {Pentimento} alt = 'Pentimento'/>
+            </Link>
+          </div>
+          <div className = 'project'>
+            <Link to = '/daisys-wine-shop'>
+              <img src = {WineShop} alt = 'Daisys Wine Shop'/>
+            </Link>
+          </div>
+          <div className = 'project'>
+            <Link to = '/sft-sansumg'>
+              <img src = {SFT_Project} alt = 'Solve For Tomorrow - Sansumg'/>
+            </Link>
+          </div>
+          <div className = 'project'>
+            <Link to = '/above-all-security'>
+              <img src = {Above_All_Security} alt = 'Above All Security'/>
+            </Link>
+          </div>
+          <div className = 'project'>
+            <Link to = '/provide-services'>
+              <img src = {Provide_Services} alt = 'Provide Services' />
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className = 'project'>
-        <Link to = '/pentimento'>
-          <img src = {Pentimento} alt = 'Pentimento'/>
-        </Link>
-      </div>
-      <div className = 'project'>
-        <Link to = '/daisys-wine-shop'>
-          <img src = {WineShop} alt = 'Daisys Wine Shop'/>
-        </Link>
-      </div>
-      <div className = 'project'>
-        <Link to = '/sft-sansumg'>
-          <img src = {SFT_Project} alt = 'Solve For Tomorrow - Sansumg'/>
-        </Link>
-      </div>
-      <div className = 'project'>
-        <Link to = '/above-all-security'>
-          <img src = {Above_All_Security} alt = 'Above All Security'/>
-        </Link>
-      </div>
-      <div className = 'project'>
-        <Link to = '/provide-services'>
-          <img src = {Provide_Services} alt = 'Provide Services' />
-        </Link>
-      </div>
-     </div>
-    </div>
     </div>
   )
 }

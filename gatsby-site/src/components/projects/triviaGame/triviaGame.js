ede188acdd4c2786d17e478d7536f9cf26c86976
Angie Spears <angie.spears809@gmail.com>
2020-06-27 19:18:45 -0400

    Add initial project pages templates

---
+++ gatsby-site/src/components/projects/triviaGame/triviaGame.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import HeaderImg from '../../../images/projects/triviaGame/trivia-game-1.jpg'
+import SectionImgOne from '../../../images/projects/triviaGame/trivia-game-2.jpg'
+
+import '../style/projectPage.scss'
+
+const TriviaGame = () => {
+  return (
+    <div>
+      <section className = 'header-placeholder'>
+        <img src = {HeaderImg} alt = 'header-img' />
+      </section>
+      <section className = 'project-details-container'>
+        <div>
+          <div className = 'details'>
+            <h1>Trivia Game</h1>
+            <div>
+              <div>
+                <div>
+                  <h2>Technologies</h2>
+                  <p>React,CSS</p>
+                </div>
+                <div>
+                  <h2>Links</h2>
+                  <a href= "#" target= "blank">Github</a>
+                  <a href= "#" target= "blank">Site</a>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h2>Lorem ipsum dolor sit amet</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className = 'project-img'>
+            <img src = {SectionImgOne} alt = 'Site img' />
+          </div>
+          <div className = 'details-2'>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default TriviaGame

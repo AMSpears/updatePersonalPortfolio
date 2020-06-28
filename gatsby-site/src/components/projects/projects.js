6f370be2fe2d7357224e99e4fb507b5d0f655e7d
Angie Spears <angie.spears809@gmail.com>
2020-06-27 21:19:12 -0400

    add title to projects

--- gatsby-site/src/components/projects/projects.js
+++ gatsby-site/src/components/projects/projects.js
@@ -18,31 +18,43 @@ const Projects = () => {
           <div className = 'project'>
             <Link to = '/frs-verizon'>
               <img src = {FRS_Project} alt = 'First Responders - Verizon'/>
+              <h2>Verizon: First Responders </h2>
+              <p>React, CSS</p>
             </Link>
           </div>
           <div className = 'project'>
             <Link to = '/pentimento'>
               <img src = {Pentimento} alt = 'Pentimento'/>
+              <h2>Pentimento </h2>
+              <p>React, CSS</p>
             </Link>
           </div>
           <div className = 'project'>
             <Link to = '/daisys-wine-shop'>
               <img src = {WineShop} alt = 'Daisys Wine Shop'/>
+              <h2>Daisys Wine Shop </h2>
+              <p>React, CSS</p>
             </Link>
           </div>
           <div className = 'project'>
             <Link to = '/sft-sansumg'>
               <img src = {SFT_Project} alt = 'Solve For Tomorrow - Sansumg'/>
+              <h2>Sansumg: Solve For Tomorrow </h2>
+              <p>React, CSS</p>
             </Link>
           </div>
           <div className = 'project'>
             <Link to = '/above-all-security'>
               <img src = {Above_All_Security} alt = 'Above All Security'/>
+              <h2>Above All Security </h2>
+              <p>React, CSS</p>
             </Link>
           </div>
           <div className = 'project'>
             <Link to = '/provide-services'>
               <img src = {Provide_Services} alt = 'Provide Services' />
+              <h2>Provide Services </h2>
+              <p>React, CSS</p>
             </Link>
           </div>
         </div>

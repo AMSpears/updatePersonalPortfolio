473bf87faa507336b62d97d878e71ee8d5b32483
Angie Spears <angie.spears809@gmail.com>
2020-06-28 13:19:27 -0400

    update projects columns

--- gatsby-site/src/components/projects/projects.js
+++ gatsby-site/src/components/projects/projects.js
@@ -15,47 +15,52 @@ const Projects = () => {
       <div>
         <div><h1>Select Works</h1></div>
         <div className = 'projects-inner-container'>
-          <div className = 'project'>
-            <Link to = '/frs-verizon'>
-              <img src = {FRS_Project} alt = 'First Responders - Verizon'/>
-              <h2>Verizon: First Responders </h2>
-              <p>React, CSS</p>
-            </Link>
+          <div>
+            <div className = 'project'>
+              <Link to = '/frs-verizon'>
+                <img src = {FRS_Project} alt = 'First Responders - Verizon'/>
+                <h2>Verizon: First Responders </h2>
+                <p>React, CSS</p>
+              </Link>
+            </div>
+            <div className = 'project'>
+              <Link to = '/pentimento'>
+                <img src = {Pentimento} alt = 'Pentimento'/>
+                <h2>Pentimento </h2>
+                <p>React, CSS</p>
+              </Link>
+            </div>
+            <div className = 'project'>
+              <Link to = '/daisys-wine-shop'>
+                <img src = {WineShop} alt = 'Daisys Wine Shop'/>
+                <h2>Daisys Wine Shop </h2>
+                <p>React, CSS</p>
+              </Link>
+            </div>
           </div>
-          <div className = 'project'>
-            <Link to = '/pentimento'>
-              <img src = {Pentimento} alt = 'Pentimento'/>
-              <h2>Pentimento </h2>
-              <p>React, CSS</p>
-            </Link>
-          </div>
-          <div className = 'project'>
-            <Link to = '/daisys-wine-shop'>
-              <img src = {WineShop} alt = 'Daisys Wine Shop'/>
-              <h2>Daisys Wine Shop </h2>
-              <p>React, CSS</p>
-            </Link>
-          </div>
-          <div className = 'project'>
-            <Link to = '/sft-sansumg'>
-              <img src = {SFT_Project} alt = 'Solve For Tomorrow - Sansumg'/>
-              <h2>Sansumg: Solve For Tomorrow </h2>
-              <p>React, CSS</p>
-            </Link>
-          </div>
-          <div className = 'project'>
-            <Link to = '/above-all-security'>
-              <img src = {Above_All_Security} alt = 'Above All Security'/>
-              <h2>Above All Security </h2>
-              <p>React, CSS</p>
-            </Link>
-          </div>
-          <div className = 'project'>
-            <Link to = '/provide-services'>
-              <img src = {Provide_Services} alt = 'Provide Services' />
-              <h2>Provide Services </h2>
-              <p>React, CSS</p>
-            </Link>
+          <div id = 'uneven-column'>
+            <div className = 'project'>
+              <Link to = '/sft-sansumg'>
+                <span></span>
+                <img src = {SFT_Project} alt = 'Solve For Tomorrow - Sansumg'/>
+                <h2>Sansumg: Solve For Tomorrow </h2>
+                <p>React, CSS</p>
+              </Link>
+            </div>
+            <div className = 'project'>
+              <Link to = '/above-all-security'>
+                <img src = {Above_All_Security} alt = 'Above All Security'/>
+                <h2>Above All Security </h2>
+                <p>React, CSS</p>
+              </Link>
+            </div>
+            <div className = 'project'>
+              <Link to = '/provide-services'>
+                <img src = {Provide_Services} alt = 'Provide Services' />
+                <h2>Provide Services </h2>
+                <p>React, CSS</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>

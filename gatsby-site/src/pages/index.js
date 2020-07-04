a3b19653951f2e7fff49f9d13d891abd8ab85b14
Angie Spears <angie.spears809@gmail.com>
2020-07-04 10:56:35 -0400

    update home page background color change func

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -10,34 +10,49 @@ import OtherProjects from '../components/otherProjects/otherProjects'
 const mainSectionStyle = {
   margin: '0',
   backgroundColor: 'white',
-  backgroundImage: 'linear-gradient(180deg, #ffffff 80%, red 100%)',
+  // backgroundImage: 'linear-gradient(180deg, #ffffff 80%, red 100%)',
   backgroundAttachment: 'fixed',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
-  minHeight: '100%'
+  minHeight: '100%',
+  transition:'0.3s all'
+
 }
+
+// exploring the idea of changing color per section
 const HomePage = () => {
-   const mainContainer = useRef()
+  const mainContainer = useRef()
+  const aboutContainer = useRef()
+  const projectsContainer = useRef()
+  const articlesContainer = useRef()
+  const otherProjectsContainer = useRef()
   // Referenced this https://stackoverflow.com/questions/57453141/using-react-hooks-to-update-w-scroll
   const [scrolling, setScrolling] = useState(false)
-  const [scrollTop, setScrollTop] = useState(20)
+  const [scrollTop, setScrollTop] = useState(0)
 
   useEffect( () => {
     const onScroll = (e) => {
-      // Define random color
-      // const colors = ['#F76259', '#92F759', '#59F7E5', '#5985F7', '#DF5FFA']
-      // const randomColor = colors[Math.floor(Math.random() * colors.length)]
 
-      // Update backgroun img color
-      if (window.pageYOffset > 800) {
-
-        // mainContainer.current.style.backgroundImage = `linear-gradient(180deg, #ffffff 70%, ${randomColor} 100%)`
-
+      if (scrollTop >= aboutContainer.current.getBoundingClientRect().top) {
+        mainContainer.current.style.backgroundImage = 'linear-gradient(180deg, #ffffff 50%, #5985F7 100%)'
       }
-      // console.log(window.pageYOffset)
+
+      if (scrollTop > projectsContainer.current.getBoundingClientRect().top) {
+        mainContainer.current.style.backgroundImage = 'linear-gradient(180deg, #ffffff 50%, #F76259 100%)'
+      }
+
+      if (scrollTop > articlesContainer.current.getBoundingClientRect().top) {
+        mainContainer.current.style.backgroundImage = 'linear-gradient(180deg, #ffffff 80%, #59F7E5 100%)'
+      }
+
+      if (scrollTop > otherProjectsContainer.current.getBoundingClientRect().top) {
+        mainContainer.current.style.backgroundImage = 'linear-gradient(180deg, #ffffff 80%, #DF5FFA 100%)'
+      }
+
       setScrollTop(e.target.documentElement.scrollTop)
       setScrolling(e.target.documentElement.scrollTop > scrollTop)
     }
+
     window.addEventListener('scroll', onScroll)
     return () => window.removeEventListener('scroll', onScroll)
   }, [scrollTop])
@@ -46,11 +61,11 @@ const HomePage = () => {
     <Layout>
       <SEO title="Home" />
       <Intro/>
-      <div style = {mainSectionStyle } in={scrolling ? 1 : 0} ref= {mainContainer}>
-        <About/>
-        <Projects/>
-        <Articles/>
-        <OtherProjects/>
+      <div  style = {mainSectionStyle} in={scrolling ? 1 : 0} ref= {mainContainer}>
+        <div ref = {aboutContainer}><About/></div>
+        <div ref = {projectsContainer}><Projects/></div>
+        <div ref = {articlesContainer}><Articles/></div>
+        <div ref = {otherProjectsContainer}><OtherProjects/></div>
       </div>
     </Layout>
   )

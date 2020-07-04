6824a1848f84d712a2f9b053280457777a17929b
Angie Spears <angie.spears809@gmail.com>
2020-07-04 12:53:11 -0400

    Add copy to verizon project

--- gatsby-site/src/components/projects/frsVerizon/frsVerizon.js
+++ gatsby-site/src/components/projects/frsVerizon/frsVerizon.js
@@ -16,33 +16,24 @@ const FrsVerizon = () => {
       <section className = 'project-details-container'>
         <div>
           <div className = 'details'>
-            <h1>Sansumg: Solve For Tomorrow</h1>
+            <h1>Verizon: First Responders</h1>
             <div>
               <div>
                 <div className = 'tech-details'>
                   <h2>Technologies</h2>
-                  <p>React,CSS</p>
-                </div>
-                <div className= 'tech-details'>
-                  <h2>Links</h2>
-                  <a href= "#" target= "blank">Github</a>
-                  <a href= "#" target= "blank">Site</a>
+                  <p>React, Sass</p>
                 </div>
               </div>
               <div>
                 <div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+                  <p>I never thought I’d be part of a Super Bowl commercial. While at R/GA I worked on an awesome website for Verizon that honored first responders.</p>
+                  <p>Verizon ran a series of commercials during the Super Bowl for people to show gratitude to first responders. I collaborated with other engineers, designers, and QA on this project.</p>
                 </div>
               </div>
             </div>
           </div>
           <div className = 'project-img'>
             <img src = {SectionImgOne} alt = 'Site img' />
-          </div>
-          <div className = 'details-2'>
-            <h2>Lorem ipsum dolor sit</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
           </div>
           <div className = 'project-img'>
             <img src = {SectionImgTwo} alt = 'Site img' />

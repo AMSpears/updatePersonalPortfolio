be3575bd3180335b067467ea9ed596cc6d6c276e
Angie Spears <angie.spears809@gmail.com>
2020-07-04 13:24:13 -0400

    update home finder copy

--- gatsby-site/src/components/projects/homeFinder/homeFinder.js
+++ gatsby-site/src/components/projects/homeFinder/homeFinder.js
@@ -14,33 +14,28 @@ const homeFinder = () => {
       <section className = 'project-details-container'>
         <div>
           <div className = 'details'>
-            <h1>Sansumg: Solve For Tomorrow</h1>
+            <h1>Home Finder</h1>
             <div>
               <div>
                 <div className = 'tech-details'>
                   <h2>Technologies</h2>
-                  <p>React,CSS</p>
+                  <p>React, Node, Express, MongoDB</p>
                 </div>
                 <div className= 'tech-details'>
                   <h2>Links</h2>
-                  <a href= "#" target= "blank">Github</a>
-                  <a href= "#" target= "blank">Site</a>
+                  <a href= "https://github.com/AMSpears/home-app-frontend" target= "blank">Github</a>
+                  <a href= "http://home-finder.surge.sh/" target= "blank">Site</a>
                 </div>
               </div>
               <div>
                 <div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+                  <p>Home Finder was a project I worked on with a team at General Assembly. For this project, we wanted to explore databases and search. We created a database where realtors could add/store/edit profiles of homes.</p>
                 </div>
               </div>
             </div>
           </div>
           <div className = 'project-img'>
             <img src = {SectionImgOne} alt = 'Site img' />
-          </div>
-          <div className = 'details-2'>
-            <h2>Lorem ipsum dolor sit</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
           </div>
         </div>
       </section>

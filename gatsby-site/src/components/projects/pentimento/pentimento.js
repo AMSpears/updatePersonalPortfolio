8d967443299a8234173bcfcd604ec70bcfeb2984
Angie Spears <angie.spears809@gmail.com>
2020-07-04 12:45:07 -0400

    update Pentimento project page

--- gatsby-site/src/components/projects/pentimento/pentimento.js
+++ gatsby-site/src/components/projects/pentimento/pentimento.js
@@ -16,22 +16,23 @@ const Pentimento = () => {
       <section className = 'project-details-container'>
         <div>
           <div className = 'details'>
-            <h1>Sansumg: Solve For Tomorrow</h1>
+            <h1>Pentimento</h1>
             <div>
               <div>
                 <div className = 'tech-details'>
                   <h2>Technologies</h2>
-                  <p>React,CSS</p>
+                  <p>Mapbox, React, Redux, Node, Express, Sequelize</p>
                 </div>
                 <div className= 'tech-details'>
                   <h2>Links</h2>
-                  <a href= "#" target= "blank">Github</a>
-                  <a href= "#" target= "blank">Site</a>
+                  <a href= "https://github.com/Phantom-Blue/phantom-blue" target= "blank">Github</a>
+                  <a href= "https://pentimentostreet.herokuapp.com/" target= "blank">Site</a>
                 </div>
               </div>
               <div>
                 <div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
+                  <p>I get to see some of the world’s best art on the streets of the neighborhood in New York. I live near Bushwick, where street art thrives. Unfortunately, street art can be up for a moment and lost forever. I worked with a team a Grace Hopper to create a web app to capture and map the best street art around the world.</p>
+                  <p>I used Mapbox in conjunction with react.js to display all artworks on a map that is center in new york city coordinates. I additionally added the view as a list functionality and gave users the ability to edit their artwork postings by updating or removing the posting.</p>
                 </div>
               </div>
             </div>
@@ -40,9 +41,13 @@ const Pentimento = () => {
             <img src = {SectionImgOne} alt = 'Site img' />
           </div>
           <div className = 'details-2'>
-            <h2>Lorem ipsum dolor sit</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
+            <div><h2>How it works</h2></div>
+            <div>
+              <p> &#8212; Any user can see and search for artwork near them. </p>
+              <p>&#8212; All users are able to create an account. </p>
+              <p> &#8212; If they have an account they can add a posting of artwork. </p>
+              <p> &#8212; They are also able to update their own artwork. </p>
+            </div>
           </div>
           <div className = 'project-img'>
             <img src = {SectionImgTwo} alt = 'Site img' />

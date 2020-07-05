e92c8fce3107379a1f89d73b6c68465027bf666e
Angie Spears <angie.spears809@gmail.com>
2020-07-05 17:37:52 -0400

    create error page

---
+++ gatsby-site/src/components/404/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ErrorPage.scss'
+const ErrorPage = () => {
+  return (
+    <div className = 'error-page-container'>
+      <div>
+        <h1>Page Not Found :/</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+    </div>
+  )
+
+}
+
+export default ErrorPage

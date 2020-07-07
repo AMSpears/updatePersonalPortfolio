f81f31c23de1e52424d24e95868bd14a51fa014b
Angie Spears <angie.spears809@gmail.com>
2020-07-06 22:09:41 -0400

    update bio img background

--- gatsby-site/src/components/about/intro.js
+++ gatsby-site/src/components/about/intro.js
@@ -1,9 +1,11 @@
 import React from "react"
 import './style/intro.scss'
-
+import BioImage from '../../images/tempBioPic.jpg'
 const Intro = () => {
   return (
     <div className = 'intro-container'>
+      <img src = {BioImage} alt= 'bio pic'/>
+
       <div>
         <div><h1>Hi there! <br/> I&apos;m Angie Spears</h1></div>
         <div><p>Full-stack Software Engineer proudly coding in New York City. I&apos;m a Grace Hopper and General Assembly Alum.</p></div>
